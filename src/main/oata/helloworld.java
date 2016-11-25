eefa58dd6cb0e6b233561371b05c0af2df9f0b1f
baimingzhang <baiming.zhang@emerson.com>
2016-11-25 14:27:53 +0800

    jenkins check

--- src/main/oata/helloworld.java
+++ src/main/oata/helloworld.java
@@ -8,7 +8,6 @@ public class helloworld {
 		Log.initLog(helloworld.class.getName());
 		Log.write(LogType.INFO, "hellowworld");
 		Log.write(LogType.INFO, "``````hellowworld1");
-		Log.write(LogType.INFO, "``````hellowworld1");
 		
 	}
 
